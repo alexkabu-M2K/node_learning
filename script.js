3ab55ce231ff428ccb738baff6514c1649edc211
Alexkabu <alexkabu@outlook.com>
2025-08-23 11:23:16 -0300

    Experimentação

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 // Módulos Node
 
 // CommonJS
-// module.exports
-
-export function somar(n1, n2) {
-    console.log (n1 + n2);
-};
 
 // export default somar;
+
+// module.exports = somar
+
+
+//export function somar(n1, n2) {
+//    console.log (n1 + n2);
+//};
+
+const fs = require('fs');
+
+fs.readFile('./arquivo.txt', 'utf-8', (erro, data) => {
+    if (erro) {
+        console.log('Erro ao ler o arquivo:', erro);
+        return;
+    }
+    console.log(`Conteudo do arquivo: ${data}`);
+});
