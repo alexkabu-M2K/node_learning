8f99da981ea6c04d22f7699bbda8bfcef05c816c
Alexkabu <alexkabu@outlook.com>
2025-08-23 11:36:24 -0300

    Criando Arquivos

--- app_02.js
+++ app_02.js
@@ -3,6 +3,22 @@
 
 // funcao(5, 10);
 
-import { somar } from './script.js';
+// import { somar } from './script.js';
 
-somar(2, 3);
+// somar(2, 3);
+
+fs.readFile('./arquivo.txt', 'utf-8', (erro, data) => {
+    if (erro) {
+        console.log('Erro ao ler o arquivo:', erro);
+        return;
+    }
+    console.log(`Conteudo do arquivo: ${data}`);
+
+    fs.writeFile('./arquivo_copia.txt', data, (erro) => {
+        if (erro) {
+            console.log('Erro ao escrever o arquivo:', erro);
+            return;
+        }
+            console.log('Arquivo copiado com sucesso!');
+        });
+    });
